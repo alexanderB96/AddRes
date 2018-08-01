70b7e00d8de0e30728ac891a362d8002a0c10be5
alexanderB96 <38032907+alexanderB96@users.noreply.github.com>
2018-08-01 13:01:06 +0400

    1 Вывод адреса

--- AddRes/AddRes/Form1.Designer.cs
+++ AddRes/AddRes/Form1.Designer.cs
@@ -28,13 +28,131 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.obl = new System.Windows.Forms.Label();
+            this.bunifuImageButton1 = new Bunifu.Framework.UI.BunifuImageButton();
+            this.rn = new System.Windows.Forms.Label();
+            this.nas = new System.Windows.Forms.Label();
+            this.yl = new System.Windows.Forms.Label();
+            this.dom = new System.Windows.Forms.Label();
+            this.kv = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(776, 21);
+            this.comboBox1.Sorted = true;
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.TextUpdate += new System.EventHandler(this.comboBox1_TextUpdate);
+            // 
+            // obl
+            // 
+            this.obl.AutoSize = true;
+            this.obl.Location = new System.Drawing.Point(12, 58);
+            this.obl.Name = "obl";
+            this.obl.Size = new System.Drawing.Size(35, 13);
+            this.obl.TabIndex = 1;
+            this.obl.Text = "label1";
+            // 
+            // bunifuImageButton1
+            // 
+            this.bunifuImageButton1.BackColor = System.Drawing.Color.SeaGreen;
+            this.bunifuImageButton1.Image = ((System.Drawing.Image)(resources.GetObject("bunifuImageButton1.Image")));
+            this.bunifuImageButton1.ImageActive = null;
+            this.bunifuImageButton1.Location = new System.Drawing.Point(746, 405);
+            this.bunifuImageButton1.Name = "bunifuImageButton1";
+            this.bunifuImageButton1.Size = new System.Drawing.Size(52, 43);
+            this.bunifuImageButton1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bunifuImageButton1.TabIndex = 2;
+            this.bunifuImageButton1.TabStop = false;
+            this.bunifuImageButton1.Zoom = 10;
+            this.bunifuImageButton1.Click += new System.EventHandler(this.bunifuImageButton1_Click);
+            // 
+            // rn
+            // 
+            this.rn.AutoSize = true;
+            this.rn.Location = new System.Drawing.Point(12, 96);
+            this.rn.Name = "rn";
+            this.rn.Size = new System.Drawing.Size(35, 13);
+            this.rn.TabIndex = 3;
+            this.rn.Text = "label2";
+            // 
+            // nas
+            // 
+            this.nas.AutoSize = true;
+            this.nas.Location = new System.Drawing.Point(12, 127);
+            this.nas.Name = "nas";
+            this.nas.Size = new System.Drawing.Size(35, 13);
+            this.nas.TabIndex = 4;
+            this.nas.Text = "label3";
+            // 
+            // yl
+            // 
+            this.yl.AutoSize = true;
+            this.yl.Location = new System.Drawing.Point(12, 155);
+            this.yl.Name = "yl";
+            this.yl.Size = new System.Drawing.Size(35, 13);
+            this.yl.TabIndex = 5;
+            this.yl.Text = "label4";
+            // 
+            // dom
+            // 
+            this.dom.AutoSize = true;
+            this.dom.Location = new System.Drawing.Point(9, 180);
+            this.dom.Name = "dom";
+            this.dom.Size = new System.Drawing.Size(35, 13);
+            this.dom.TabIndex = 6;
+            this.dom.Text = "label5";
+            // 
+            // kv
+            // 
+            this.kv.AutoSize = true;
+            this.kv.Location = new System.Drawing.Point(9, 206);
+            this.kv.Name = "kv";
+            this.kv.Size = new System.Drawing.Size(35, 13);
+            this.kv.TabIndex = 7;
+            this.kv.Text = "label6";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.kv);
+            this.Controls.Add(this.dom);
+            this.Controls.Add(this.yl);
+            this.Controls.Add(this.nas);
+            this.Controls.Add(this.rn);
+            this.Controls.Add(this.bunifuImageButton1);
+            this.Controls.Add(this.obl);
+            this.Controls.Add(this.comboBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label obl;
+        private Bunifu.Framework.UI.BunifuImageButton bunifuImageButton1;
+        private System.Windows.Forms.Label rn;
+        private System.Windows.Forms.Label nas;
+        private System.Windows.Forms.Label yl;
+        private System.Windows.Forms.Label dom;
+        private System.Windows.Forms.Label kv;
     }
 }
 
--- AddRes/AddRes/Form1.cs
+++ AddRes/AddRes/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NUnit.Framework;
+using suggestionscsharp;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,77 @@ namespace AddRes
 {
     public partial class Form1 : Form
     {
+        string addres = null;
+        string nasp;
         public Form1()
         {
             InitializeComponent();
+            
+        }
+
+        private void comboBox1_TextUpdate(object sender, EventArgs e)
+        {
+             
+            var token = "888f110f8d1cc69dbdb07839d780e7f29f36fa13";
+            var url = "https://suggestions.dadata.ru/suggestions/api/4_1/rs";
+            var api = new SuggestClient(token, url);
+            var query = comboBox1.Text;
+            var response = api.QueryAddress(query);
+            var address_data = response.suggestions[0].data;
+            if (address_data.settlement != null)
+            {
+                nasp = (address_data.settlement);
+            }
+            else if (address_data.city != null)
+            {
+                nasp = (address_data.city);
+            }
+            obl.Text = (address_data.region);
+            rn.Text = (address_data.area);
+            nas.Text = nasp;
+            yl.Text = (address_data.street);
+            dom.Text = (address_data.house);
+            kv.Text = (address_data.flat);
+            comboBox1.Items.Add(string.Join("\n", response.suggestions[0]));
+
+          
+        }
+
+        private void bunifuCustomLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            addres = comboBox1.Text;
+            //криво но работает (получается что дубль запрос)
+            var token = "888f110f8d1cc69dbdb07839d780e7f29f36fa13";
+            var url = "https://suggestions.dadata.ru/suggestions/api/4_1/rs";
+            var api = new SuggestClient(token, url);
+            var query = comboBox1.Text;
+            var response = api.QueryAddress(query);
+            var address_data = response.suggestions[0].data;
+            if (address_data.settlement != null)
+            {
+                nasp = (address_data.settlement);
+            }
+            else if (address_data.city != null)
+            {
+                nasp = (address_data.city);
+            }
+            obl.Text = (address_data.region);
+            rn.Text = (address_data.area);
+            nas.Text = nasp;
+            yl.Text = (address_data.street);
+            dom.Text = (address_data.house);
+            kv.Text = (address_data.flat);
+        }
+
+        private void bunifuImageButton1_Click(object sender, EventArgs e)
+        {
+            comboBox1.Text = "";
+            comboBox1.Items.Clear();
         }
     }
 }
